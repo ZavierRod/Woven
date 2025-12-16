e9bc4e1ecdbf667eb62cdd3e370cf31ad2a78232
ZavierRod <zavier_rod@berkeley.edu>
2025-12-16 08:33:33 -0800

    Initial Commit

---
+++ Woven/WovenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WovenApp.swift
+//  Woven
+//
+//  Created by Zavier Rodrigues on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WovenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WovenTests/WovenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WovenTests.swift
+//  WovenTests
+//
+//  Created by Zavier Rodrigues on 12/16/25.
+//
+
+import Testing
+@testable import Woven
+
+struct WovenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
